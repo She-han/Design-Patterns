8ae69bc1d779c54f99820580ae766277878e8d15
Shehan Garusingarachchige <shehangarusinghe@gmail.com>
2025-08-08 23:46:09 +0530

    factory method test

---
+++ Factory Method Pattern/PersonFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Factory_Method_Pattern
+{
+    internal class PersonFactory
+    {
+        private int count = 0;
+
+        public Person CreatePerson(string name) {
+
+            Person person = new Person();
+            person.id = count;
+            person.name = name;
+            count++;
+            return person;
+        }
+    }
+}
---
+++ Factory Method Pattern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory_Method_Pattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonFactory pfac = new PersonFactory();
+            Person p1 = pfac.CreatePerson("shehan");
+            Person p2 = pfac.CreatePerson("saman");
+
+            Console.WriteLine("person1 id = " + p1.id + ", name = " + p1.name);
+            Console.WriteLine("person2 id = " + p2.id + ", name = " + p2.name);
+        }
+    }
+}
